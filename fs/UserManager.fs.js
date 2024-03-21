ff38d4c7f57af18ac48aa2a45d0c91d26ac035c3
Damian <damiruggi@gmail.com>
2024-03-21 12:27:30 -0300

    Implemente express, nodemon y genere los archivos server.js, router.js, .gitignore y modifique el archivo UserManager.fs.js para la consigna de este sprint3

--- fs/UserManager.fs.js
+++ fs/UserManager.fs.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const crypto = require("crypto");
+import fs from "fs";
+import crypto from "crypto";
 
-module.exports = class UsersManager {
+class UsersManager {
   constructor() {
     this.path = "./fs/files/users.json";
     this.init();
@@ -17,121 +17,81 @@ module.exports = class UsersManager {
       console.log("El archivo ya existe!");
     }
   }
-  
+
   async create(data) {
     try {
       // Validar campos obligatorios
       if (!data.photo || !data.email || !data.password || !data.role) {
-        throw new Error("Los campos 'photo', 'email', 'password' y 'role' son obligatorios.");
+        throw new Error(
+          "Los campos 'photo', 'email', 'password' y 'role' son obligatorios."
+        );
       }
 
       if (!data.email) {
         throw new Error("Ingrese el email");
       } else {
-        let all = await fs.promises.readFile(this.path, "utf-8");
-        all = JSON.parse(all);
-        
-        // Verificar si el usuario ya existe
-        const existingUser = all.find(user => user.email === data.email);
-        if (existingUser) {
-          throw new Error("El usuario ya existe");
-        }
-
         const user = {
           id: crypto.randomBytes(12).toString("hex"),
-          photo: data.photo || "https://cdn-icons-png.freepik.com/512/266/266033.png",
+          photo:
+            data.photo ||
+            "https://cdn-icons-png.freepik.com/512/266/266033.png",
           email: data.email,
           password: data.password,
-          role: data.role,
+          role: data.role || "Editor",
         };
-        
+        let all = await fs.promises.readFile(this.path, "utf-8");
+        all = JSON.parse(all);
         all.push(user);
-        await fs.promises.writeFile(this.path, JSON.stringify(all, null, 2));
-        console.log({ created: user.id });
+        all = JSON.stringify(all, null, 2);
+        await fs.promises.writeFile(this.path, all);
         return user;
       }
     } catch (error) {
       console.log(error);
     }
   }
-  
-  async read() {
+
+  async read(role) {
     try {
-      const all = await fs.promises.readFile(this.path, "utf-8");
-      console.log(JSON.parse(all));
-      return JSON.parse(all);
+      let all = await fs.promises.readFile(this.path, "utf-8");
+      all = JSON.parse(all);
+      role && (all = all.filter((each) => each.role === role));
+      return all;
     } catch (error) {
       console.log(error);
+      return error;
     }
   }
-  
+
   async readOne(id) {
     try {
-      const all = await fs.promises.readFile(this.path, "utf-8");
-      const user = JSON.parse(all).find((each) => each.id === id);
-      if (!user) {
-        throw new Error("Usuario no encontrado");
-      } else {
-        console.log(user);
-        return user;
-      }
+      let all = await fs.promises.readFile(this.path, "utf-8");
+      all = JSON.parse(all);
+      let note = all.find((each) => each.id === id);
+      return note;
     } catch (error) {
       console.log(error);
+      return error;
     }
   }
-  
+
   async destroy(id) {
     try {
       let all = await fs.promises.readFile(this.path, "utf-8");
       all = JSON.parse(all);
-      let user = all.find((each) => each.id === id);
-      if (!user) {
-        throw new Error("Usuario no encontrado");
-      } else {
+      let note = all.find((each) => each.id === id);
+      if (note) {
         let filtered = all.filter((each) => each.id !== id);
         filtered = JSON.stringify(filtered, null, 2);
         await fs.promises.writeFile(this.path, filtered);
-        console.log({ deleted: user.id });
-        return user;
       }
+      return note;
     } catch (error) {
       console.log(error);
+      return error;
     }
   }
-};
-
-async function test() {
-  try {
-    const users = new module.exports();
-    await users.create({
-      photo: "https://ceslava.s3-accelerate.amazonaws.com/2012/12/foto-perfil.jpg",
-      email: "damiruggi@gmail.com",
-      password: "123456",
-      role: "Admin",
-    });
-    await users.create({
-      photo: "https://wl-genial.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg",
-      email: "antumoles@gmail.com",
-      password: "567890",
-      role: "Admin",
-    });
-    await users.create({
-      photo: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
-      email: "lalalala@gmail.com",
-      password: "234sdff",
-      role: "Editor",
-    });
-    await users.create({
-      photo: "https://i.pinimg.com/550x/8d/e7/fa/8de7fa2af12330350613ede63532c4fb.jpg",
-      email: "morena@hotmail.com",
-      password: "fdsfdsda23",
-      role: "Editor",
-    });
-    await users.read();
-    //await users.destroy("c678b56d14c4808beba07dd4");
-    //await users.readOne("722a0fdab7dc518fb4103e51");
-  } catch (error) {
-    console.log(error);
-  }
 }
-test();
+
+const usersManager = new UsersManager();
+export default usersManager;
---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import usersManager from "./fs/UserManager.fs.js";
+const server = express();
+const port = 8080;
+const ready = () => console.log("server ready on port " + port);
+server.listen(port, ready);
+
+server.use(express.urlencoded({ extended: true }));
+server.get("/", async (requerimientos, respuesta) => {
+  try {
+    return respuesta.status(200).json({
+      response: "API",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return respuesta.status(500).json({
+      response: "API ERROR",
+      success: false,
+    });
+  }
+});
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const { role } = req.query;
+    const all = await usersManager.read(role);
+    if (all.length !== 0) {
+      return res.status(200).json({
+        response: all,
+        role,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
+
+server.get("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const one = await usersManager.readOne(uid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
