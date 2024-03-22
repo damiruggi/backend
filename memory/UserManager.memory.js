f6d7e7af368251ba458186c81f52bdd257e13d6e
Damian <damiruggi@gmail.com>
2024-03-22 09:25:50 -0300

    Agregue el readOne y el destroy de usuarios

--- memory/UserManager.memory.js
+++ memory/UserManager.memory.js
@@ -7,25 +7,25 @@ class UserManager {
     // Verifica si están todos los campos necesarios
     if (!data.photo || !data.email || !data.password || !data.role) {
       console.error("Falta uno o más campos obligatorios.");
-      return;
+      return null;
     }
 
     // Verifica el formato de la foto
     if (!/\.(jpg|png)$/i.test(data.photo)) {
       console.error("El formato de la foto debe ser JPG o PNG.");
-      return;
+      return null;
     }
 
     // Verificar el email
     if (!isValidEmail(data.email)) {
       console.error("El email proporcionado no es válido.");
-      return;
+      return null;
     }
 
     // Verificar la seguridad del password
     if (data.password.length < 6) {
       console.error("La contraseña debe tener al menos 6 caracteres.");
-      return;
+      return null;
     }
 
     const user = {
@@ -37,10 +37,36 @@ class UserManager {
     };
     UserManager.#users.push(user);
     console.log("Usuario creado con éxito.");
+    return user.id; // Devolver el ID del usuario creado
   }
 
   read() {
     return UserManager.#users;
+  }
+
+  readOne(id) {
+    try {
+      const one = UserManager.#users.find((each) => each.id === id);
+      if (!one) {
+        throw new Error("No existe el usuario");
+      } else {
+        return one;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  destroy(id) {
+    try {
+      const userToDelete = this.readOne(id); // Obtener el usuario a eliminar
+      if (userToDelete) {
+        UserManager.#users = UserManager.#users.filter((each) => each.id !== id);
+        console.log("Usuario eliminado");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
@@ -51,18 +77,20 @@ function isValidEmail(email) {
 
 const gestorDeUsuarios = new UserManager();
 
-gestorDeUsuarios.create({
+const id1 = gestorDeUsuarios.create({
   photo: "https://ceslava.s3-accelerate.amazonaws.com/2012/12/foto-perfil.jpg",
   email: "damiruggi@gmail.com",
   password: "123456",
-  role: "Admin"
+  role: "Admin",
 });
 
-gestorDeUsuarios.create({
+const id2 = gestorDeUsuarios.create({
   photo: "https://wl-genial.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg",
   email: "antumoles@gmail.com",
   password: "567890",
-  role: "Admin"
+  role: "Admin",
 });
 
-console.log(gestorDeUsuarios.read());
+console.log(gestorDeUsuarios.read(id1, id2));
+//console.log(gestorDeUsuarios.readOne(id2));
+//gestorDeUsuarios.destroy(id2);
