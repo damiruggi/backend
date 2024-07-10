c319e0d9aa623792a6b06e67d5a33b929ce89a73
Damián Rodríguez <el_ruggi@hotmail.com>
2024-07-10 10:30:10 -0300

    Sprint11 (#23)
    * Cree la rama del sprint 7
    
    * Implemente en users y products models el Type junto con los index y populate, tambien los testee en postman y compass para corroborar el funcionamiento
    
    * Agregue el archivo CartsManager.fs.js
    
    * Actualice el archivo readme
    
    * Quite el archivo ticket
    
    * hola
    
    * Ajuste e hice andar nuevamente visual studio
    
    * actualizacion
    
    * Quite el .env
    
    * Instale nuevamente el .env y lo reasigne al gitignore
    
    * Quite tambien el gitignore
    
    * Implemente bcript y passport
    
    * Instale google passport y token, tambien estan aplicados
    
    * Agregue la categoria en la creacion de productos
    
    * Implementacion de JWT
    
    * Arregle la redireccion al loguearse con google y tambien que reconozca el user al loguearse
    
    * actualizacion
    
    * Implemente los servicios
    
    * Actualice los passport para poder loguearme
    
    * Actualizacion
    
    * Actualizacion de token
    
    * Ajsute las variables de entorno
    
    * Actualzacion
    
    * Implemente DTO, mocks y nodemailer
    
    * Actualice el archivo readme
    
    * Implemente faker y agregue 1000 productos
    
    * Implemnente el manejo de errores con compression
    
    * Ajuste de error en el export default del errors.js
    
    * fixes
    
    * fixes
    
    * Implemente winston y artillery
    
    ---------
    
    Co-authored-by: Damian <damiruggi@gmail.com>

--- server.js
+++ server.js
@@ -1,37 +1,71 @@
 import environment from "./src/utils/env.util.js";
 import express from "express";
-import morgan from "morgan";
+import cluster from "cluster";
+import { cpus } from "os";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import compression from "express-compression";
-
+//import morgan from "morgan"; // Asegúrate de importar morgan
 import argsUtil from "./src/utils/args.util.js";
-
 import indexRouter from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
+import winston from "./src/middlewares/winston.mid.js";
 
-//http server
-const server = express();
 const port = environment.PORT || argsUtil.p;
-const ready = async () => console.log("server ready on port " + port);
-server.listen(port, ready);
 
-//middlewares
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-server.use(express.static(__dirname + "/public"));
-server.use(morgan("dev"));
-server.use(cookieParser(environment.SECRET_COOKIE));
-server.use(cors({ origin: true, credentials: true }));
-server.use(
+if (cluster.isPrimary) {
+  const numOfProc = cpus().length;
+
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numOfProc; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  // http server
+  const server = express();
+
+  const ready = async () => console.log("Server ready on port " + port);
+
+  // middlewares
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(express.static(__dirname + "/public"));
+  server.use(winston);
+  server.use(cookieParser(environment.SECRET_COOKIE));
+  server.use(cors({ origin: true, credentials: true }));
+  server.use(
     compression({
-    brotli: { enabled: true, zlib: {} },
+      brotli: { enabled: true, zlib: {} },
     })
-    );
+  );
 
-//endpoints
-server.use("/", indexRouter);
-server.use(errorHandler);
-server.use(pathHandler);
+  // endpoints
+  server.use("/", indexRouter);
+  server.use(errorHandler);
+  server.use(pathHandler);
+
+  // start server
+  server.listen(port, ready);
+
+  // handle worker errors
+  process.on('uncaughtException', (err) => {
+    console.error(`Uncaught exception: ${err.message}`);
+    process.exit(1); // exit the worker to let cluster handle the restart
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    process.exit(1); // exit the worker to let cluster handle the restart
+  });
+}
